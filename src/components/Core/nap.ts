8854a932755ef98f375f6ac4014fa5b31ab6e250
Jean-Denis Vauguet <jd@vauguet.fr>
2018-01-18 00:08:37 +0100

    Cleaning Core/nap up

--- src/components/Core/nap.ts
+++ src/components/Core/nap.ts
@@ -5,54 +5,20 @@ import { AppState, Reducer } from "types"
 import Model from "model"
 import Records from "models/Records"
 
+
 // Next-action-predicate aka. internal side-effect handler.
 //
-// Computes any required side-effect and pushes it down the pipe.
+// Computes any required side-effect and pushes it down the pipe as a reducer.
+//
 // A side-effect could be:
 // - a mutation proposal
 // - an emulated user-action (resulting in a mutation proposal)
-// - an effect on some external asset with no internal consequence whatsoever
 //
-// The last use-case is more suited to sources-less drivers so should be avoided.
-// Emulated user-action are better handled outside the intent() function due to
-// stream wiring. Therefore, such actions must be pre-processed here as a
-// mutation proposal and exposed as such.
-// export default function nap(app_state$) {
-//   // Typing is over but stats (model decoration) are still pending: trigger
-//   // updates and re-rendering with a fake keypress.
-//   const compute_wpm$ = app_state$
-//     .filter(_ => {
-//       return Model.Singleton.get().isDone()
-//         && !Model.Singleton.get().attributes.stop
-//     })
-//     .map(app_state => (new TypingAction).process())
-//
-//   // Once everything is over, including stats computation/display, let's reset
-//   // everything after a short delay so the user may retry.
-//   const auto_reset$ = app_state$
-//     .filter(_ => {
-//       return Model.Singleton.get().isDone()
-//         && Model.Singleton.get().attributes.stop
-//     })
-//     .compose(delay(2000))
-//     .map(_ => Model.Singleton.clear())
-//
-//   // User has stopped, must reset state.
-//   const manual_reset$ = app_state$
-//     .filter(_ => {
-//       return Model.Singleton.get().hasStopped()
-//     })
-//     .map(_ => Model.Singleton.clear())
-//
-//   return xs.merge(
-//     compute_wpm$,
-//     auto_reset$,
-//     manual_reset$
-//   ).startWith(undefined)
-// }
-
-
+// As next-action-predicates listen for state$, they have the potential for
+// unleashing infinite loop doom: proceed with caution, use restrictive safe
+// guards in the form of .filter() statements.
 export default function nap(state$): Stream<Reducer> {
+  // NOTE: return xs.merge(…) if multiple reducers.
   return state$
     // NOTE: we're interested in inspecting state$ upon a specific event from
     // state$ being emitted, which leaves the possibility state$ itself will
