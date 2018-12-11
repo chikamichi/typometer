4ee69d55844707293ca7fab735d281fe0a46d3af
Jean-Denis Vauguet <jd@vauguet.fr>
2018-12-11 07:01:28 +0100

    Code cleanup

--- src/components/Core/model.ts
+++ src/components/Core/model.ts
@@ -3,43 +3,31 @@ import sampleCombine from 'xstream/extra/sampleCombine'
 
 import { Reducer } from "typometer/types"
 import State from 'typometer/models/State'
-import { CoreActions } from './nap'
 import InitialStateReducer from 'typometer/reducers/InitialState'
 import * as Actions from 'typometer/actions/Core'
 
-// Model: map actions to state reducers.
+
+export interface CoreActions {
+  success$: Stream<boolean>,
+  computeRecords$: Stream<boolean>
+}
+
+type Tuple = [any, State] 
+
 export default function model(actions: CoreActions, state$: Stream<State>): Stream<Reducer> {
   const initialState$ = xs.of(InitialStateReducer)
 
   const success$ = actions.success$.map(Actions.Success)
 
-  // // Triggers when the user made a(t least one) mistake while typing the text.
-  // const textStatusKO$ = actions.textStatusKO$
-  //   .map(_ => {
-  //     return ((prevState: State) => {
-  //       return prevState
-  //     }) as Reducer
-  //   })
-
-  // // Triggers when the user starts typing the text.
-  // const textStatusEditing$ = actions.textStatusEditing$
-  //   .map(_ => {
-  //     return ((prevState: State) => {
-  //       return prevState
-  //     }) as Reducer
-  //   })
-
-  // const computeRecords$ = actions.computeRecords$.map(Actions.ComputeRecords)
-  const computeRecords$ = actions.computeRecords$.compose(sampleCombine(state$))
-                                                 .map(([_, state]) => state.data.records)
-                                                 .map(Actions.ComputeRecords)
+  const computeRecords$ = actions.computeRecords$
+    .compose(sampleCombine(state$))
+    .map(([_, state]: Tuple) => state.data.records)
+    .map(Actions.ComputeRecords)
 
   const reducers: Stream<Reducer>[] = []
   reducers.push(
     initialState$,
     success$,
-    // textStatusKO$,
-    // textStatusEditing$,
     computeRecords$
   )
   return xs.merge(...reducers)
--- src/components/Core/nap.ts
+++ src/components/Core/nap.ts
@@ -1,14 +1,8 @@
-import { Stream, MemoryStream } from "xstream"
+import { MemoryStream } from "xstream"
 
 import State from "typometer/models/State"
+import { CoreActions } from 'typometer/components/Core/model'
 
-
-export interface CoreActions {
-  // textStatusEditing$: Stream<boolean>
-  // textStatusKO$: Stream<boolean>
-  success$: Stream<boolean>,
-  computeRecords$: Stream<boolean>
-}
 
 // Next-action-predicate aka. internal side-effect handler.
 //
@@ -23,42 +17,12 @@ export interface CoreActions {
 // guards in the form of .filter() statements.
 export default function nap(state$: MemoryStream<State>): CoreActions {
   return {
-    // Triggers "true" when the user is done typing the whole text \o/
     success$: state$
       .filter(state => state.isDone() && state.hasNoStats())
       .map(_ => true),
       
-    // // Triggers "true" when the user made a(t least one) mistake while typing the text.
-    // textStatusKO$: state$
-    //   .filter(state => {
-    //     const model = Model(state)
-    //     return !!model.hasError()
-    //   })
-    //   .map(_=> true),
-
-    // // Triggers "true" when the user starts typing the text.
-    // textStatusEditing$: state$
-    //   .filter(state => {
-    //     const model = Model(state)
-    //     return !!model.textBeingEdited()
-    //   })
-    //   .map(_=> true),
-
-    // // Triggers "true" when metrics must be computed.
-    // // NOTE: we're interested in inspecting state$ upon a specific event from
-    // // state$ being emitted, which leaves the possibility state$ itself will
-    // // have evolved between the moment the triggering event is filtered against
-    // // below and the moment state$ is processed within Metrics.Records().
-    // // It shouldn't be a problem really for we're computing records, but let's
-    // // be aware of that potential pitfall and the likely need for a better
-    // // strategy/fix here.
     computeRecords$: state$
       .filter(state => state.isDoneDone() && state.hasNoStats())
       .map(_ => true)
-      // .map(_ => {
-      //   // TODO: not the best design sending the whole track of AppState objects.
-      //   // Best would be to accumulate and compare only current run with current record.
-      //   return Metrics.Records(state$)
-      // })
   }
 }
